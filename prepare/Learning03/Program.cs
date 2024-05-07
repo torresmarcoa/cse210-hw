cf7cd8ce63a1eaccf31b5f9ef90eed1a0c0be3b9
torresmarcoa <marcotorresaceves@gmail.com>
2024-05-06 23:38:55 -0600

    Learning 3 activities

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new Fraction(6);
+        fraction2.SetTop(5);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(6,7);
+        fraction3.SetTop(3);
+        fraction3.SetBottom(4);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+       Fraction fraction4 = new Fraction(1,3);
+       Console.WriteLine(fraction4.GetFractionString());
+       Console.WriteLine(fraction4.GetDecimalValue());
+
     }
 }
